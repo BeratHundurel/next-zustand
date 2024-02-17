70b6e701931015021778e9a2797ec9e02c1c9284
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-02-17 18:00:43 +0300

    adding zustand store

---
+++ lib/store.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand'
+import { v4 as uuidv4 } from 'uuid'
+export type Status = 'TODO' | 'IN_PROGRESS' | 'DONE'
+export type Task = {
+  id: string
+  title: string
+  description?: string
+  status: Status
+}
+export type State = {
+  tasks: Task[]
+}
+export type Actions = {
+  addTask: (title: string, description?: string) => void
+  removeTask: (id: string) => void
+  updateTask: (id: string, status: Status) => void
+}
+export const useTaskStore = create<State & Actions>()(set => ({
+  tasks: [],
+  addTask: (title: string, description?: string) =>
+    set((state: State) => ({
+      ...state.tasks,
+      tasks: [...state.tasks, { id: '123', title, description, status: 'TODO' }]
+    })),
+  removeTask: (id: string) =>
+    set((state: State) => ({
+      tasks: state.tasks.filter(task => task.id !== id)
+    })),
+  updateTask: (id: string, status: Status) =>
+    set((state: State) => ({
+      tasks: state.tasks.map(task =>
+        task.id === id ? { ...task, status } : task
+      )
+    }))
+}))
